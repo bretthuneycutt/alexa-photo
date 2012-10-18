f4cd6517bce6e487eaf91ccc62aa7e41e6f9e250
Brett Huneycutt <brett@1000memories.com>
2012-10-17 22:19:53 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlexaPhoto::Application.config.secret_token = 'bc6cecd6e23569fafa83fe2bc5c8145177f14f18cbcafef166631ea62fb23cac1088957fc7698803f37e39fdb7b88035621a7356e92a88b76acbf0f0051e560d'
