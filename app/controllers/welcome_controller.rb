7843b5c47bd84ee3742f85033588e35db03997d1
Brett Huneycutt <brett@1000memories.com>
2012-10-20 23:08:11 -0700

    add basic pages for family, kids and new born photos

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,20 @@
 class WelcomeController < ApplicationController
 	def index
 	end
+
+  def kids
+    @urls = (1..48).map {|i| URL.assets("kids/#{i}.jpg") }
+    @urls.concat (1..24).map {|i| URL.assets"kids/v#{i}.jpg" }
+    render "photography_type"
+  end
+
+  def newborns
+    @urls = (1..13).map {|i| URL.assets("newborns/#{i}.jpg")}
+    render "photography_type"
+  end
+
+  def families
+    @urls = (2..4).map {|i| URL.assets("families/#{i}.jpg")}
+    render "photography_type"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -48,6 +48,10 @@ AlexaPhoto::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+  match 'kids' => 'welcome#kids'
+  match 'newborns' => 'welcome#newborns'
+  match 'families' => 'welcome#families'
+
   root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
