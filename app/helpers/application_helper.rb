380c68d9aab27c2c0d98f86688632dadceae350d
Brett Huneycutt <brett@1000memories.com>
2012-10-21 23:11:33 -0700

    nav links are highlighted when active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def inside_layout layout = 'application', &block
     render :inline => capture_haml(&block), :layout => "layouts/#{layout}"
   end
+
+  def nav_link_active?(link)
+    case link
+    when :details
+      "active"  if action_name == "photography_details"
+    else
+      "active"  if action_name == link.to_s
+    end
+  end
+
+  def nav_link_options(link)
+    {:class => 'active'}  if nav_link_active?(link)
+  end
 end
